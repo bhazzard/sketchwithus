2352b9f0bb22386684dfc68e3fef0c2ef39ebf2f
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-10-04 20:27:52 -0400

    Added very basic canvas demo

---
+++ canvas.js
@@ -0,0 +1,62 @@
+(function() {
+	var canvas = document.getElementById('sketch'),
+		penDown = false,
+		offsetX = $(canvas).offset().left,
+		offsetY = $(canvas).offset().top,
+		context;
+	
+	if (canvas.getContext) {
+		context = canvas.getContext('2d');
+	}
+	
+	function mouseMove(event) {
+		var x = event.pageX,
+			y = event.pageY;
+		
+		if (penDown) {
+			context.lineTo(x - offsetX, y - offsetY);
+			context.stroke();
+		}
+	};
+	
+	function mouseEnter(event) {
+		var x = event.pageX,
+			y = event.pageY;
+		
+		if (penDown) {
+			context.beginPath();
+			context.moveTo(x - offsetX, y - offsetY);
+		}
+	};
+	
+	function mouseDown(event) {
+		if (!penDown) {
+			penDown = true;
+			mouseEnter(event);
+		}
+	};
+	
+	function mouseOut() {
+		context.closePath();
+	};
+	
+	function mouseUp() {
+		if (penDown) {
+			penDown = false;
+			mouseOut();
+		}
+	};
+	
+	$(canvas).bind({
+		mousedown: mouseDown,
+		mouseenter: mouseEnter,
+		mousemove: mouseMove,
+		mouseup: mouseUp,
+		mouseout: mouseOut
+	});
+	
+	$(document).bind({
+		mousedown: mouseDown,
+		mouseup: mouseUp
+	});
+})();
