1823de64e318e5cbdc4f78e53aa51ece8b38f8c6
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-10-09 18:16:20 -0400

    Added line class with tests for interpolate method

---
+++ public/javascripts/line.js
@@ -0,0 +1,42 @@
+define(function() {
+	function interpolateX(x, start, end) {
+		return Math.round(start.y + (x - start.x) * ((end.y - start.y)/(end.x - start.x)));
+	};
+	
+	function interpolateY(y, start, end) {
+		return Math.round(((end.x - start.x) * (y - start.y))/(end.y - start.y) + start.x);
+	};
+
+	function Line(start, end) {
+		this.start = start || { x: 0, y: 0 };
+		this.end = end || this.start;
+	};
+
+	Line.prototype.interpolate = function() {
+		var start = this.start,
+			end = this.end,
+			xDelta = Math.abs(end.x - start.x),
+			yDelta = Math.abs(end.y - start.y),
+			xSign = Math.round(xDelta/(end.x - start.x)),
+			ySign = Math.round(yDelta/(end.y - start.y)),
+			points = [start],
+			x, y;
+		
+		if (xDelta > 0 && yDelta > 0) {
+			if (xDelta > yDelta) {
+				for (x = start.x + xSign; x !== end.x; x += xSign) {
+					points.push({ x: x, y: interpolateX(x, start, end) });
+				}
+			} else if (yDelta >= xDelta) {
+				for (y = start.y + ySign; y !== end.y; y += ySign) {
+					points.push({ x: interpolateY(y, start, end), y: y });
+				}
+			}
+			points.push(end);
+		}
+		
+		return points;
+	};
+	
+	return Line;
+});
---
+++ public/javascripts/test/line.js
@@ -0,0 +1,56 @@
+require(['line'], function(Line) {
+	module('Line');
+
+	test('default', function() {
+		var line = new Line();
+		deepEqual(line.start, { x: 0, y: 0 }, 'start');
+		deepEqual(line.end, { x: 0, y: 0 }, 'end');
+	});
+	
+	test('interpolate', function() {
+		var line = new Line({ x: 1, y: 1 });
+		deepEqual(line.interpolate(), [{ x: 1, y: 1 }], 'point');
+		
+		line = new Line({ x: 1, y: 1 }, { x: 5, y: 3 });
+		deepEqual(line.interpolate(), [
+			{ x: 1, y: 1 },
+			{ x: 2, y: 2 },
+			{ x: 3, y: 2 },
+			{ x: 4, y: 3 },
+			{ x: 5, y: 3 }
+		], 'horizontal');
+		
+		line = new Line({ x: 1, y: 1 }, { x: 3, y: 5 });
+		deepEqual(line.interpolate(), [
+			{ x: 1, y: 1 },
+			{ x: 2, y: 2 },
+			{ x: 2, y: 3 },
+			{ x: 3, y: 4 },
+			{ x: 3, y: 5 }
+		], 'vertical');
+		
+		line = new Line({ x: 1, y: 1 }, { x: 2, y: 2 });
+		deepEqual(line.interpolate(), [
+			{ x: 1, y: 1 },
+			{ x: 2, y: 2 }
+		], 'consecutive');
+		
+		line = new Line({ x: 5, y: 3 }, { x: 1, y: 1 });
+		deepEqual(line.interpolate(), [
+			{ x: 5, y: 3 },
+			{ x: 4, y: 3 },
+			{ x: 3, y: 2 },
+			{ x: 2, y: 2 },
+			{ x: 1, y: 1 }
+		], 'reverse horizontal');
+		
+		line = new Line({ x: 3, y: 5 }, { x: 1, y: 1 });
+		deepEqual(line.interpolate(), [
+			{ x: 3, y: 5 },
+			{ x: 3, y: 4 },
+			{ x: 2, y: 3 },
+			{ x: 2, y: 2 },
+			{ x: 1, y: 1 }
+		], 'reverse vertical');
+	});
+});
