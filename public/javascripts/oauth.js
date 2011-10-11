5e37a132db321bf918cac89f3f73aab896f75e7e
Brad Fitzwater <brad.fitzwater@gmail.com>
2011-10-10 22:47:15 -0400

    Formatting

--- public/javascripts/oauth.js
+++ public/javascripts/oauth.js
@@ -1,13 +1,13 @@
 function user(profile){
-	var obj = {};
-	obj.img = $("<img />")
-		.attr("src", "http://graph.facebook.com/" + profile.id + "/picture")
-		.attr("width", "36")
-		.attr("width", "36")
-		.addClass("profile-image");
-	obj.name = $("<h3 />")
-		.text(profile.name);
-	return obj;
+  var obj = {};
+  obj.img = $("<img />")
+    .attr("src", "http://graph.facebook.com/" + profile.id + "/picture")
+    .attr("width", "36")
+    .attr("width", "36")
+    .addClass("profile-image");
+  obj.name = $("<h3 />")
+    .text(profile.name);
+  return obj;
 };
 
 $(document).ready(function(){
@@ -29,31 +29,31 @@ $(document).ready(function(){
   FB.getLoginStatus(function(response) {
     if (response.status === "connected") {
       FB.api('/me', function(profile) {
-				var u = user(profile);
-				var logout = $("<a />")
-					.attr("href", "javascript:void(0)")
-					.text("Logout?")
-					.addClass("logout");
-				$("<div />")
-					.attr("id", "user")
-					.append(u.img)
-					.append(u.name)
-					.append(logout)
-					.prependTo("body");
+        var u = user(profile);
+        var logout = $("<a />")
+          .attr("href", "javascript:void(0)")
+          .text("Logout?")
+          .addClass("logout");
+        $("<div />")
+          .attr("id", "user")
+          .append(u.img)
+          .append(u.name)
+          .append(logout)
+          .prependTo("body");
       });
- 
-			FB.api('/me/friends?access_token=' + response.session.access_token, function(friends){
-				$("<h2 />").text(friends.data.length + " Friends").appendTo("#friends");
-				$.each(friends.data, function(i, friend){
-					var u = user(friend);
-					var clear = $("<div />").css("clear:both");
-					$("<li />")
-						.append(u.img)
-						.append(u.name)
-						.append(clear)
-						.appendTo("#friends");
-				});
-			});
+
+      FB.api('/me/friends?access_token=' + response.session.access_token, function(friends){
+        $("<h2 />").text(friends.data.length + " Friends").appendTo("#friends");
+        $.each(friends.data, function(i, friend){
+          var u = user(friend);
+          var clear = $("<div />").css("clear:both");
+          $("<li />")
+            .append(u.img)
+            .append(u.name)
+            .append(clear)
+            .appendTo("#friends");
+        });
+      });
     } else {
       $("#fb-login-wrapper").show();
       FB.Event.subscribe('auth.login', function(auth) {
