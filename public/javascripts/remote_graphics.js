1e2a449b43bf308601aec160676b9bb263bba152
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-10-15 17:51:11 -0400

    Get things running again

--- public/javascripts/remote_graphics.js
+++ public/javascripts/remote_graphics.js
@@ -1,11 +1,15 @@
 define(['artist', 'graphics'], function (Artist, Graphics) {
+
+  //TODO: will the hard-coded port bite us in the ass?
+  var socketServer = typeof(location) === 'undefined' ? 'http://localhost:8000/' : '/';
+
 	function RemoteGraphics(sketchpad_id, io) {
 	  this._io = io;
 		this._sketchpad_id = sketchpad_id;
 	};
 
 	RemoteGraphics.prototype.listen = function(context) {
-		var socket = this._io.connect('/' + this._sketchpad_id),
+		var socket = this._io.connect(socketServer + this._sketchpad_id),
 		    artists = {};
 		    
 		socket.on('join', function(ids) {
@@ -19,9 +23,7 @@ define(['artist', 'graphics'], function (Artist, Graphics) {
 		});
 
 		socket.on('draw', function(data) {
-			var a = artists[data.id],
-			method = a[data.invocation.method];
-			method.apply(a, data.invocation.arguments);
+			artists[data.id].execute(data.command);
 		});
     
     this._socket = socket;
