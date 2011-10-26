fb372191c97468ffe07d7a9ffacd4b8df43c1331
Brian Hazzard <bhazzard@gmail.com>
2011-10-26 00:23:54 -0400

    most of the basic ui for chat functionality

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,12 +3,32 @@
     socket = io.connect('/chatrooms/' + sketchpad_id);
     
   socket.on('chat', function(chat) {
-    if (console) {
-      console.log(chat);
-    }
+    $('#chat ul').trigger('chat', chat);
   });
 
   exports.chat = function(text) {
     socket.emit('chat', text);
   };
 })(window);
+
+(function($) {
+  function appendChat(chat) {
+      $('#chat ul').append($('<li class="from ' + chat.id + '">' + chat.text + '<li>')); 
+  }
+
+  $(document).ready(function() {
+    $('#chat')
+      .append($('<ul>'))
+      .append($('<input>').attr('type', 'text'));
+
+    $('#chat input').bind('keyup', 'return', function() {
+      chat(this.value);
+      appendChat({ id: 'me', text: this.value });
+      this.value = '';
+    });
+
+    $('#chat ul').bind('chat', function(event, chat) {
+      appendChat(chat);
+    });
+  });
+})(jQuery);
